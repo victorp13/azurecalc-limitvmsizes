394e870d0f0f78f3a1b4d7defa64869470f7059f
Victor Pikula <vipiku@microsoft.com>
2020-10-18 01:10:08 +0200

    Added Memory range

--- content-scripts/content.js
+++ content-scripts/content.js
@@ -1,26 +1,51 @@
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  $("body").prepend(
-    `<div>${request.minSize} - ${request.maxSize}</div>`
-  );
-  for (let i = 1; i < parseInt(request.minSize); i++) {
-    $(`[name='size'] option:contains(': ${i} Core')`).hide();
-    $(`[name='size'] option:contains(': ${i} vCPU')`).hide();
+  const largestCpu = 416;
+  const largestMem = 11400;
+  const minCpu = parseInt(request.minCpu);
+  const maxCpu = parseInt(request.maxCpu);
+  const minMem = parseInt(request.minMem);
+  const maxMem = parseInt(request.maxMem);
+
+  for (let i = 1; i <= largestCpu; i++) {
+    $(`[name='size'] option:contains(': ${i} Core')`).show();
+    $(`[name='size'] option:contains(': ${i} vCPU')`).show();
   }
-  for (let i = parseInt(request.maxSize) + 1; i <= 416; i++) {
-    $(`[name='size'] option:contains(': ${i} Core')`).hide();
-    $(`[name='size'] option:contains(': ${i} vCPU')`).hide();
+
+  for (let i = 1; i <= largestMem; i++) {
+    $(`[name='size'] option:contains(', ${i} GB RAM')`).show();
   }
-  // for (let i = request.minSize; i <= request.maxSize; i++) {
-  //   $(`[name='size'] option:contains(': ${i} Core')`).show();
-  //   $(`[name='size'] option:contains(': ${i} vCPU')`).show();
-  // }
-  //$(`[name='size'] option:contains(': ${request.size} Core')`).hide();
-  //$(`[name='size'] option:contains(': ${request.size} vCPU')`).hide();
+
+  switch (request.action) {
+    case "limit":
+      $("body").prepend(
+        `<div>${request.minCpu} - ${request.maxCpu}</div>`
+      );
+
+      for (let i = 1; i < minCpu; i++) {
+        $(`[name='size'] option:contains(': ${i} Core')`).hide();
+        $(`[name='size'] option:contains(': ${i} vCPU')`).hide();
+      }
+
+      for (let i = maxCpu + 1; i <= largestCpu; i++) {
+        $(`[name='size'] option:contains(': ${i} Core')`).hide();
+        $(`[name='size'] option:contains(': ${i} vCPU')`).hide();
+      }
+
+      for (let i = 1; i < minMem; i++) {
+        $(`[name='size'] option:contains(', ${i} GB RAM')`).hide();
+      }
+
+      for (let i = maxMem + 1; i <= largestMem; i++) {
+        $(`[name='size'] option:contains(', ${i} GB RAM')`).hide();
+      }
+  }
+
   $(`[name='size'] option`).each(function () {
     if ($(this).css('display') != 'none') {
       $(this).prop("selected", true);
       return false;
     }
   });
+
   sendResponse({ fromcontent: "This message is from content.js" });
 });
--- popup/popup.js
+++ popup/popup.js
@@ -1,27 +1,43 @@
-const sendMessageId = document.getElementById("sendmessageid");
-if (sendMessageId) {
-  sendMessageId.onclick = function () {
-    const minSize = document.getElementById('minSize').value
-    const maxSize = document.getElementById('maxSize').value
+const buttonLimit = document.getElementById("buttonLimit");
+if (buttonLimit) {
+  buttonLimit.onclick = function () {
+    const minCpu = document.getElementById('minCpu').value;
+    const maxCpu = document.getElementById('maxCpu').value;
+    const minMem = document.getElementById('minMem').value;
+    const maxMem = document.getElementById('maxMem').value;
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
       chrome.tabs.sendMessage(
         tabs[0].id,
         {
-          minSize: minSize,
-          maxSize: maxSize,
-          tabId: tabs[0].id
+          minCpu: minCpu,
+          maxCpu: maxCpu,
+          minMem: minMem,
+          maxMem: maxMem,
+          action: "limit"
         },
         function (response) {
           console.log("message with url sent");
           window.close();
         }
       );
-      function guidGenerator() {
-        const S4 = function () {
-          return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
-        };
-        return (S4() + S4() + "-" + S4() + "-" + S4() + "-" + S4() + "-" + S4() + S4() + S4());
-      }
+    });
+  };
+}
+
+const buttonReset = document.getElementById("buttonReset");
+if (buttonReset) {
+  buttonReset.onclick = function () {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.tabs.sendMessage(
+        tabs[0].id,
+        {
+          action: "reset"
+        },
+        function (response) {
+          console.log("message with url sent");
+          window.close();
+        }
+      );
     });
   };
 }
