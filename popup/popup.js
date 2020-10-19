29dfd8533ba7c32136d8374425bcc37516a44068
Victor Pikula <vipiku@microsoft.com>
2020-10-19 21:14:11 +0200

    Added sliders

--- popup/popup.js
+++ popup/popup.js
@@ -1,10 +1,10 @@
 const buttonLimit = document.getElementById("buttonLimit");
 if (buttonLimit) {
   buttonLimit.onclick = function () {
-    const minCpu = document.getElementById('minCpu').value;
-    const maxCpu = document.getElementById('maxCpu').value;
-    const minMem = document.getElementById('minMem').value;
-    const maxMem = document.getElementById('maxMem').value;
+    const minCpu = sliderCpu.noUiSlider.get()[0];
+    const maxCpu = sliderCpu.noUiSlider.get()[1];
+    const minMem = sliderMem.noUiSlider.get()[0];
+    const maxMem = sliderMem.noUiSlider.get()[1];
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
       chrome.tabs.sendMessage(
         tabs[0].id,
@@ -13,7 +13,7 @@ if (buttonLimit) {
           maxCpu: maxCpu,
           minMem: minMem,
           maxMem: maxMem,
-          action: "limit"
+          action: "limit",
         },
         function (response) {
           console.log("message with url sent");
@@ -31,7 +31,7 @@ if (buttonReset) {
       chrome.tabs.sendMessage(
         tabs[0].id,
         {
-          action: "reset"
+          action: "reset",
         },
         function (response) {
           console.log("message with url sent");
@@ -41,3 +41,99 @@ if (buttonReset) {
     });
   };
 }
+
+var sliderCpu = document.getElementById("sliderCpu");
+
+noUiSlider.create(sliderCpu, {
+  start: [1, 416],
+  connect: true,
+  tooltips: true,
+  format: {
+    to: function (value) {
+      return value;
+    },
+    from: function (value) {
+      return Number(value);
+    },
+  },
+  snap: true,
+  range: {
+    min: [1],
+    "6.25%": [2],
+    "12.5%": [4],
+    "18.75%": [6],
+    "25%": [8],
+    "31.25%": [16],
+    "37.5%": [20],
+    "43.75%": [24],
+    "50%": [32],
+    "56.25%": [48],
+    "62.5%": [64],
+    "68.75%": [72],
+    "75%": [80],
+    "81.25%": [128],
+    "87.5%": [208],
+    "93.75%": [340],
+    max: [416],
+  },
+});
+
+//alert(txt);
+// slidercpu.noUiSlider.on("set", function (values, handle) {
+//   var values = slidercpu.noUiSlider.get();
+//   var value = Number(values[handle]);
+//   var steps = slidercpu.noUiSlider.steps();
+//   var step = steps[handle];
+//   console.log("prev: " + (value - step[0]));
+//   console.log("next: " + (value + step[1]));
+// });
+
+var sliderMem = document.getElementById("sliderMem");
+
+noUiSlider.create(sliderMem, {
+  start: [1, 11400],
+  connect: true,
+  tooltips: true,
+  format: {
+    to: function (value) {
+      return value;
+    },
+    from: function (value) {
+      return Number(value);
+    },
+  },
+  snap: true,
+  range: {
+    min: [1],
+    "2%": [2],
+    "4%": [4],
+    "6%": [6],
+    "8%": [8],
+    "10%": [14],
+    "12%": [16],
+    "14%": [28],
+    "16%": [32],
+    "18%": [48],
+    "20%": [56],
+    "24%": [64],
+    "28%": [80],
+    "32%": [112],
+    "36%": [128],
+    "40%": [219],
+    "44%": [224],
+    "48%": [256],
+    "50%": [438],
+    "52%": [448],
+    "54%": [512],
+    "58%": [875],
+    "60%": [1024],
+    "65%": [1750],
+    "70%": [1792],
+    "75%": [2048],
+    "80%": [2850],
+    "85%": [3800],
+    "90%": [3892],
+    "95%": [5700],
+    max: [11400],
+  },
+});
